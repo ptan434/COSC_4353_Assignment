48d49f8698b096664da022a0a23c7ef87825ae25
ptan434 <48169623+ptan434@users.noreply.github.com>
2021-03-19 22:37:42 -0500

    Add files via upload

--- server/index.js
+++ server/index.js
@@ -7,7 +7,6 @@ const passport = require("passport");
 const fs = require("fs");
 const pool = require("./db");
 const initializePassport = require("./passportConfig");
-const dotenv = require("dotenv").config();
 var userData = require("./userObject");
 
 
@@ -20,7 +19,7 @@ app.use(express.static(dirname));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(session({
-      secret: process.env.SESSION_SECRET,
+      secret: "SESSION_SECRET",
       resave: false,
       saveUninitialized: false
     })
