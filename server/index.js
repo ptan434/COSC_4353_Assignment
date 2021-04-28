c67e86cc859612e705667327d27052bbd2556011
ptan434 <48169623+ptan434@users.noreply.github.com>
2021-04-28 13:57:14 -0500

    Add files via upload

--- server/index.js
+++ server/index.js
@@ -1,13 +1,13 @@
-const express = require('express');
-const path = require('path');
-const bcrypt = require('bcrypt');
+const express = require("express");
+const path = require("path");
+const bcrypt = require("bcrypt");
 const session = require("express-session");
 const flash = require("express-flash");
 const passport = require("passport");
 const fs = require("fs");
 const pool = require("./db");
 const initializePassport = require("./passportConfig");
-
+const { reset } = require("nodemon");
 
 const app = express();
 const PORT = process.env.PORT || 9000;
@@ -32,11 +32,15 @@ app.set('view engine', 'html');
 app.set('views', dirname);
 
 app.get('/index', (req, res) => {
-    res.sendFile(path.join(dirname + '/index.html'));
+  res.render(path.join(dirname + '/index.html'), {message:req.flash('error')});
 });
 
 app.get("/login", (req, res) => {
-  res.sendFile(path.join(dirname + '/components/login.html'));
+  res.render(path.join(dirname + '/components/login.html'), {message:req.flash('error')});
+});
+
+app.get('/reset_password', (req, res) => {
+  res.render(path.join(dirname + '/components/reset_password.html'), {message:req.flash('error')});
 });
 
 app.get("/fuel_quote", checkNotAuthenticated, async(req, res) => {
@@ -74,25 +78,236 @@ app.get("/fuel_history", checkNotAuthenticated, async(req, res) => {
   res.render(path.join(dirname + '/components/fuel_history.html'), {fuelHistory:fuelHistory});
 });
 
+app.get("/dashboard", checkNotAuthenticated, async(req, res) => {
+  console.log(req.isAuthenticated());
+  var check = await checkProfile(req.user.user_id);
+  if (check.length > 0)
+    res.render(path.join(dirname + '/components/dashboard.html'), {fullName:check[0].full_name});
+  else
+    res.redirect("/create_profile");
+});
+
 app.get("/profile", checkNotAuthenticated, async(req, res) => {
   console.log(req.isAuthenticated());
   var check = await checkProfile(req.user.user_id);
   if (check.length > 0)
-    res.sendFile(path.join(dirname + '/components/profile.html'));
+  {
+    var profile = await checkProfile(req.user.user_id);
+    var primaryAddressID = profile[0].primary_address_id;
+    var secondaryAddressID = profile[0].secondary_address_id;
+    var primaryAddress = await getAddress(primaryAddressID);
+    var pAddress = primaryAddress[0].address;
+    var pCity = primaryAddress[0].city;
+    var pState = primaryAddress[0].state;
+    var pZipcode = primaryAddress[0].zipcode;
+    var secondaryAddress, sAddress, sCity, sState, sZipcode;
+    if (secondaryAddressID != null)
+    {
+      secondaryAddress = await getAddress(secondaryAddressID);
+      sAddress = secondaryAddress[0].address;
+      sCity = secondaryAddress[0].city;
+      sState = secondaryAddress[0].state;
+      sZipcode = secondaryAddress[0].zipcode;
+      res.render(path.join(dirname + '/components/profile.html'), {
+        fullName:profile[0].full_name,
+        pAddress:pAddress,
+        pCity:pCity,
+        pState:pState,
+        pZipcode:pZipcode,
+        sAddress:sAddress,
+        sCity:sCity,
+        sState:sState,
+        sZipcode:sZipcode
+      });
+    }
+    else
+      res.render(path.join(dirname + '/components/profile.html'), {
+        fullName:profile[0].full_name,
+        pAddress:pAddress,
+        pCity:pCity,
+        pState:pState,
+        pZipcode:pZipcode,
+        sAddress:"not registered",
+        sCity:"empty",
+        sState:"empty",
+        sZipcode:"empty"
+      });
+  }
   else
     res.redirect("/create_profile");
+});
+
+app.get("/edit_profile", checkNotAuthenticated, async(req, res) => {
+  console.log(req.isAuthenticated());
+  var profile = await checkProfile(req.user.user_id);
+  var primaryAddressID = profile[0].primary_address_id;
+  var secondaryAddressID = profile[0].secondary_address_id;
+  var states = await getState();
+  var primaryAddress = await getAddress(primaryAddressID);
+  var pAddress = primaryAddress[0].address.trim();
+  var pCity = primaryAddress[0].city.trim();
+  var pState = primaryAddress[0].state.trim();
+  var pZipcode = primaryAddress[0].zipcode.trim();
+  var secondaryAddress, sAddress, sCity, sState, sZipcode;
+  if (secondaryAddressID != null)
+  {
+    secondaryAddress = await getAddress(secondaryAddressID);
+    sAddress = secondaryAddress[0].address.trim();
+    sCity = secondaryAddress[0].city.trim();
+    sState = secondaryAddress[0].state.trim();
+    sZipcode = secondaryAddress[0].zipcode.trim();
+    res.render(path.join(dirname + '/components/edit_profile.html'), {
+      fullName:profile[0].full_name,
+      pAddress:pAddress,
+      pCity:pCity,
+      pState:pState,
+      pZipcode:pZipcode,
+      sAddress:sAddress,
+      sCity:sCity,
+      sState:sState,
+      sZipcode:sZipcode,
+      states:states,
+      message:req.flash('error')
+    });
+  }
+  else
+    res.render(path.join(dirname + '/components/edit_profile.html'), {
+      fullName:profile[0].full_name,
+      pAddress:pAddress,
+      pCity:pCity,
+      pState:pState,
+      pZipcode:pZipcode,
+      sAddress:"not registered",
+      sCity:"empty",
+      sState:"empty",
+      sZipcode:"empty",
+      states:states,
+      message:req.flash('error')
+    });
+});
+
+app.get("/logout", (req, res) => {
+  req.logOut();
+  req.flash('error', "You have successfully signed out");
+  res.redirect("/index");
 });
 
 app.get("/create_profile", checkNotAuthenticated, async(req, res) => {
   console.log(req.isAuthenticated());
   var check = await checkProfile(req.user.user_id);
+  var states = await getState();
   if (check.length > 0)
-    res.redirect("/profile");
+    res.redirect("/dashboard");
   else
-    res.sendFile(path.join(dirname + '/components/create_profile.html'));
+    res.render(path.join(dirname + '/components/create_profile.html'), {states:states, message:req.flash('error')});
 });
 
-app.post("/fuel_quote", (req, res) => {
+app.post("/edit_profile", checkNotAuthenticated, async(req, res) => {
+  let {fullName, addressOne, cityOne, stateOne, zipCodeOne,
+    addressTwo, cityTwo, stateTwo, zipCodeTwo} = req.body;
+  var profile = await checkProfile(req.user.user_id);
+  var primaryAddressID = profile[0].primary_address_id;
+  var secondaryAddressID = profile[0].secondary_address_id;
+  var secondaryAddress;
+  var newAddressID;
+  let errors = [];
+
+  fullName = fullName.trim();
+  addressOne = addressOne.trim();
+  cityOne = cityOne.trim();
+  stateOne = stateOne.trim();
+  zipCodeOne = zipCodeOne.trim();
+  addressTwo = addressTwo.trim();
+  cityTwo = cityTwo.trim();
+  stateTwo = stateTwo.trim();
+  zipCodeTwo = zipCodeTwo.trim();
+
+  if (addressTwo == "not registered")
+    addressTwo = "";
+  if (cityTwo == "empty")
+    cityTwo = "";
+  if (zipCodeTwo == "empty")
+    zipCodeTwo = "";
+
+  if (fullName.length > 50)
+    errors.push({message: "Maximum length for name is 50"});
+  if (addressOne.length > 100)
+    errors.push({message: "Maximum length for address is 100"});
+  if (cityOne.length > 100)
+    errors.push({message: "Maximum length for city is 100"});
+  if (zipCodeOne.length < 5)
+    errors.push({message: "Minimum length for zip code is 5"});
+  if (zipCodeOne.length > 9)
+    errors.push({message: "Maximum length for zip code is 9"});
+
+  if (fullName == "" || addressOne == "" || cityOne == "" || stateOne == "null" || zipCodeOne == "")
+    errors.push({message: "Please enter all required fields for primary address"});
+  else if (addressTwo != "" && cityTwo != "" && stateTwo != "empty" && zipCodeTwo != "")
+    secondaryAddress = true;
+  else if (addressTwo != "" || cityTwo != "" || stateTwo != "empty" || zipCodeTwo != "")
+    errors.push({message: "Please enter all required fields for secondary address"});
+  else
+    secondaryAddress = false;
+
+  if (errors.length > 0) {
+    req.flash('error', errors[0].message);
+    console.log(errors[0].message);
+    res.redirect("/edit_profile");
+  }
+  else {
+    await pool.query(
+      `UPDATE address
+        SET address = '${addressOne}',
+            city = '${cityOne}',
+            state = '${stateOne}',
+            zipcode = '${zipCodeOne}'
+        WHERE address_id = '${primaryAddressID}';`
+    );
+    if (secondaryAddress) {
+      if (secondaryAddressID != null) {
+        await pool.query(
+          `UPDATE address
+            SET address = '${addressTwo}',
+                city = '${cityTwo}',
+                state = '${stateTwo}',
+                zipcode = '${zipCodeTwo}'
+            WHERE address_id = '${secondaryAddressID}';`
+        );
+      }
+      else {
+        newAddressID = await pool.query(
+          `INSERT INTO address (address, city, state, zipcode)
+          VALUES ('${addressTwo}', '${cityTwo}', '${stateTwo}', '${zipCodeTwo}')
+          RETURNING address_id`
+        );
+        await pool.query(
+          `UPDATE profile
+            SET secondary_address_id = '${newAddressID.rows[0].address_id}'
+            WHERE user_id = '${profile[0].user_id}'`
+        );
+      }
+    }
+    else {
+      if (secondaryAddressID != null) {
+        await pool.query(`
+          UPDATE profile
+            SET secondary_address_id = NULL
+            WHERE user_id = '${profile[0].user_id}';
+          DELETE FROM address
+          WHERE address_id = '${secondaryAddressID}';`
+        );
+      }
+    }
+    await pool.query(
+    `UPDATE profile
+      SET full_name = '${fullName}'
+      WHERE user_id = '${profile[0].user_id}'`
+    );
+    res.redirect("/profile");
+  }
+});
+
+app.post("/fuel_quote", async(req, res) => {
   let {gallonsRequested, deliveryAddress, deliveryDate, suggestedPrice, total} = req.body;
   let errors = [];
   var passed = true;
@@ -100,7 +315,7 @@ app.post("/fuel_quote", (req, res) => {
     errors.push({message: "Please enter all fields"});}
   
     if (passed) {
-      pool.query(
+      await pool.query(
         `INSERT INTO fuel_quote (user_id, delivery_address, delivery_date, gallons_requested, suggested_price_per_gallon, total)
             VALUES ($1, $2, $3, $4, $5, $6)`,
         [req.user.user_id, deliveryAddress, deliveryDate, gallonsRequested, suggestedPrice.substring(1), total.substring(1)],
@@ -113,7 +328,7 @@ app.post("/fuel_quote", (req, res) => {
         }
       );
     }
-})
+});
 
 app.post("/register", async(req, res) => {
     let {registerUserID, registerEmail, registerPass, registerConfirmPass, termCondition} = req.body;
@@ -127,7 +342,8 @@ app.post("/register", async(req, res) => {
       errors.push({message: "Please review our terms and conditions"});}
 
     if (errors.length > 0) {
-      console.log(errors);
+      req.flash('error', errors[0].message);
+      res.redirect("/index");
     }
     else {
       hashedPassword = await bcrypt.hash(registerPass, 10);
@@ -136,22 +352,20 @@ app.post("/register", async(req, res) => {
       var check;
       if (passed) {
         check = await checkID(registerUserID);
-        console.log("check id length: " + check.length);
         if (check.length > 0) {
-          console.log("User ID already registered.");
+          req.flash('error', "User ID already registered.");
           passed = false;
         }
       }
       if (passed) {
         check = await checkEmail(registerEmail);
-        console.log("check email length: " + check.length);
         if (check.length > 0) {
-          console.log("Email already registered.");
+          req.flash('error', "Email already registered.");
           passed = false;
         }
       }
       if (passed) {
-        pool.query(
+        await pool.query(
           `INSERT INTO users (user_id, password, email, last_login)
               VALUES ($1, $2, $3, NULL)`,
           [registerUserID, hashedPassword, registerEmail],
@@ -159,12 +373,59 @@ app.post("/register", async(req, res) => {
             if (err) {
               throw err;
             }
-            console.log("You are now registered. Please log in to complete your profile.");
+            req.flash('error', "You are now registered. Please log in to complete your profile.");
             res.redirect("/login");
           }
         );
       }
+      if (!passed) {
+        res.redirect("/index");
+      }
     }
+});
+
+app.post("/reset_password", async(req, res) => {
+  let {inputUserID, resetPass, resetConfirmPass} = req.body;
+  let errors = [];
+
+  if (!inputUserID || !resetPass || !resetConfirmPass) {
+    errors.push({message: "Please enter all fields"});}
+  if (resetPass !== resetConfirmPass) {
+    errors.push({message: "Passwords do not match"});}
+
+  if (errors.length > 0) {
+    req.flash('error', errors[0].message);
+    res.redirect("/reset_password");
+  }
+  else {
+    hashedPassword = await bcrypt.hash(resetPass, 10);
+    var passed = true;
+    var check;
+    if (passed) {
+      check = await checkID(inputUserID);
+      if (check.length == 0) {
+        req.flash('error', "User ID not found.");
+        passed = false;
+      }
+    }
+    if (passed) {
+      await pool.query(
+        `UPDATE users
+          SET password = '${hashedPassword}'
+          WHERE user_id ='${inputUserID}'`,
+        (err, results) => {
+          if (err) {
+            throw err;
+          }
+          req.flash('error', "Successfully reset password.");
+          res.redirect("/index");
+        }
+      );
+    }
+    if (!passed) {
+      res.redirect("/reset_password");
+    }
+  }
 });
 
 app.post("/create_profile", async(req, res) => {
@@ -176,6 +437,16 @@ app.post("/create_profile", async(req, res) => {
   var primaryAddressID;
   var secondaryAddressID;
   let errors = [];
+
+  fullName = fullName.trim();
+  addressOne = addressOne.trim();
+  cityOne = cityOne.trim();
+  stateOne = stateOne.trim();
+  zipCodeOne = zipCodeOne.trim();
+  addressTwo = addressTwo.trim();
+  cityTwo = cityTwo.trim();
+  stateTwo = stateTwo.trim();
+  zipCodeTwo = zipCodeTwo.trim();
 
   if (fullName.length > 50)
     errors.push({message: "Maximum length for name is 50"});
@@ -198,7 +469,8 @@ app.post("/create_profile", async(req, res) => {
     secondaryAddress = false;
   
   if (errors.length > 0) {
-    console.log(errors);
+    req.flash('error', errors[0].message);
+    res.redirect("/create_profile");
   }
   else {
     queryAddress =
@@ -214,28 +486,26 @@ app.post("/create_profile", async(req, res) => {
         RETURNING address_id`;
       secondaryAddressID = await pool.query(queryAddress);
 
-      pool.query(
+      await pool.query(
         `INSERT INTO profile (user_id, primary_address_id, secondary_address_id, full_name)
           VALUES ('${userID}', '${primaryAddressID.rows[0].address_id}', '${secondaryAddressID.rows[0].address_id}', '${fullName}')`,
           (err, results) => {
             if (err) {
               throw err;
             }
-            console.log("Congratulation, you have completed your registration.");
-            res.redirect("/profile");
+            res.redirect("/dashboard");
           }
       );
     }
     else
-      pool.query(
+      await pool.query(
         `INSERT INTO profile (user_id, primary_address_id, secondary_address_id, full_name)
           VALUES ('${userID}', '${primaryAddressID.rows[0].address_id}', NULL, '${fullName}')`,
           (err, results) => {
             if (err) {
               throw err;
             }
-            console.log("Congratulation, you have completed your registration.");
-            res.redirect("/profile");
+            res.redirect("/dashboard");
           }
       );
   }
@@ -246,8 +516,8 @@ app.post("/login",
     failureRedirect: "/index",
     failureFlash: true
   }),
-  function(req, res) {
-    pool.query(
+  async(req, res) => {
+    await pool.query(
       `UPDATE users
         SET last_login = CURRENT_TIMESTAMP
         WHERE user_id = '${req.user.user_id}'`,
@@ -255,7 +525,7 @@ app.post("/login",
           if (err) {
             throw err;
           }
-          res.redirect("/profile");
+          res.redirect("/dashboard");
         }
     );
   });
@@ -265,8 +535,8 @@ app.post("/new_user",
     failureRedirect: "/index",
     failureFlash: true
   }),
-  function(req, res) {
-    pool.query(
+  async(req, res) => {
+    await pool.query(
       `UPDATE users
         SET last_login = CURRENT_TIMESTAMP
         WHERE user_id = '${req.user.user_id}'`,
@@ -326,12 +596,17 @@ const checkAddress = async(userID) => {
   return response.rows;
 }
 
-const getAddress = async(primaryAddressID) => {
+const getAddress = async(addressID) => {
   var response = await pool.query(
     `SELECT * FROM address
       WHERE address_id = $1`,
-    [primaryAddressID]
+    [addressID]
   );
+  return response.rows;
+}
+
+const getState = async() => {
+  var response = await pool.query(`SELECT * FROM state`);
   return response.rows;
 }
 
